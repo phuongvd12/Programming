5fd4e2007cd3e2616df1b124b48400040b750cbd
phuongvd12 <ducphuongsis@gmail.com>
2025-02-05 17:46:51 +0700

    input in c++

---
+++ cpp/example/cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int tuoi;
+
+	cout << "Nhap so tuoi cua ban" << endl;
+	cin >> tuoi;
+	cout << "Ban sinh nam: " << 2025 - tuoi << endl;
+	
+	return 0;
+}
--- cpp/example/var.cpp
+++ cpp/example/var.cpp
@@ -7,8 +7,16 @@ int main(void)
 	int x = 3;
 	int y = 7;
 	int z = x + y;
+	int so = 3;
+	float sothapphan = 3.14;
+	char kytu = 'p';
+	string chuoikitu = "xin chao Viet Nam";
 
 	cout << z << endl;
-
+	cout << so << endl;
+	cout << sothapphan << endl;
+	cout << kytu << endl;
+	cout << chuoikitu << endl;
+	
 	return 0;
 }
