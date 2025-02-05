2b585d5ea006677d5461df716ede12833080361e
phuongvd12 <ducphuongsis@gmail.com>
2025-02-05 20:26:43 +0700

    function in c++

--- cpp/example/cin.cpp
+++ cpp/example/cin.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,6 +10,17 @@ int main()
 	cout << "Nhap so tuoi cua ban" << endl;
 	cin >> tuoi;
 	cout << "Ban sinh nam: " << 2025 - tuoi << endl;
-	
+
+	cout << "Nhap ngay thang nam sinh cua ban" << endl;
+	int date, month, year;
+	cin >> date >> month >> year;
+	cout << "Ban sinh vao " << date << "/" << month << "/" << year << endl;
+
+	cout << "Ban ten gi?" << endl;
+	string ten;
+	cin.ignore(1);
+	getline(cin, ten);
+	cout << "Xin chao " << ten << endl;
+
 	return 0;
 }
---
+++ cpp/example/func.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void xinchao(void); // function prototype
+
+int main()
+{
+	xinchao();
+
+	return 0;
+}
+
+
+void xinchao(void) // function definition
+{
+	cout << "xin chao cac ban" << endl;
+}
